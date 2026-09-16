4b1452b82760eff2e440c65c80a8e7c06c057015
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-09-16 14:26:38 +0200

    chore: fix home VM tests

--- feature/home/src/test/kotlin/com/rossomak/flashcards/feature/home/HomeViewModelTest.kt
+++ feature/home/src/test/kotlin/com/rossomak/flashcards/feature/home/HomeViewModelTest.kt
@@ -1,11 +1,25 @@
 package com.rossomak.flashcards.feature.home
 
+import com.rossomak.flashcards.core.domain.repository.FakeFlashcardRepository
+import com.rossomak.flashcards.core.domain.repository.FakeUserFavoritesRepository
+import com.rossomak.flashcards.core.domain.usecase.ObserveFavoriteItemsUseCase
+import com.rossomak.flashcards.testutil.MainDispatcherRule
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class HomeViewModelTest {
 
-    private fun createViewModel(): HomeViewModel = HomeViewModel()
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val flashcardRepository = FakeFlashcardRepository()
+    private val userFavoritesRepository = FakeUserFavoritesRepository()
+    private val observeFavoriteItems = ObserveFavoriteItemsUseCase(userFavoritesRepository, flashcardRepository)
+
+    private fun createViewModel(): HomeViewModel = HomeViewModel(observeFavoriteItems)
 
     @Test
     fun `initial state exposes the default home screen state`() {
