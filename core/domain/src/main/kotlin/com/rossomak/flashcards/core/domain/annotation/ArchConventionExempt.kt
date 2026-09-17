8c338570be93f874d0d046e3ff6cb05ac5735b78
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-09-18 01:04:59 +0200

    setup: add custom Konsist exemption annotation

---
+++ core/domain/src/main/kotlin/com/rossomak/flashcards/core/domain/annotation/ArchConventionExempt.kt
@@ -0,0 +1,9 @@
+package com.rossomak.flashcards.core.domain.annotation
+
+/**
+ * Opts a class or function out of one or more architecture-convention Konsist rules.
+ * Always pass a reason; grep for this annotation to see what's exempted and why.
+ */
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class ArchConventionExempt()
